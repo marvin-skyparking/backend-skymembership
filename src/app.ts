8b69d1ede754330e1bd26cc034aa3d1400d56244
Marvin6262 <marvinaj12@gmail.com>
2024-10-10 16:05:42 +0700

    fixing cors

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,25 @@ import cors from 'cors';
 const app = express().disable('x-powered-by');
 
 // CORS options to allow requests from localhost:9000
+const allowedOrigins = [
+  'http://localhost:9001',
+  'https://apipaymentservice.skyparking.online'
+];
+
 const corsOptions = {
-  origin: 'http://localhost:9001', // Replace with your allowed origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: (
+    origin: string | undefined,
+    callback: (error: Error | null, success?: boolean) => void
+  ) => {
+    // Check if the origin is in the allowed origins list or if it's undefined (for non-browser requests)
+    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Deny the request
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  // Removed allowedHeaders
 };
 
 app.use(cors(corsOptions)); // Use the defined CORS options
