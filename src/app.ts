d648deade659bb7b2b8fa1d76a7c51456494dc00
Marvin6262 <marvinaj12@gmail.com>
2024-12-11 15:47:59 +0700

    fixing cors

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,7 @@ const allowedOrigins = [
   'http://localhost:9001',
   'http://localhost:9002',
   'https://apipaymentservice.skyparking.online',
-  'https://dev-membership.skyparking.online',
-  '*'
+  'https://dev-membership.skyparking.online'
 ];
 
 const corsOptions = {
