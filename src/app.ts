e4995cbc25dea8fa12baa633862161568b371c44
Marvin6262 <marvinaj12@gmail.com>
2024-12-16 15:06:12 +0700

    fixing create product

--- src/app.ts
+++ src/app.ts
@@ -8,21 +8,43 @@ import cors from 'cors';
 // Initialize express app
 const app = express().disable('x-powered-by');
 
-// Allow requests from all origins (CORS configuration)
-app.use(
-  cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  })
-);
+// CORS options to allow requests from localhost:9000
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:9001',
+  'http://localhost:9002',
+  'https://apipaymentservice.skyparking.online',
+  'https://dev-membership.skyparking.online',
+  'https://apiintegration.skyparking.online'
+];
+
+const corsOptions = {
+  // origin: (
+  //   origin: string | undefined,
+  //   callback: (error: Error | null, success?: boolean) => void
+  // ) => {
+  //   // Check if the origin is in the allowed origins list or if it's undefined (for non-browser requests)
+  //   if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+  //     callback(null, true); // Allow the request
+  //   } else {
+  //     callback(new Error('Not allowed by CORS')); // Deny the request
+  //   }
+  // },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedOrigins
+  // Removed allowedHeaders
+};
+
+app.use(cors(corsOptions)); // Use the defined CORS options
 app.use(express.json());
 
-// Initialize Swagger if enabled
+// Initialize Swagger
 const enableSwagger = process.env.ENABLE_SWAGGER === 'true';
 
 if (enableSwagger) {
+  // Serve Swagger API documentation
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
@@ -30,7 +52,7 @@ if (enableSwagger) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// User routes
+// User routes (Example)
 app.use('/v1', indexRoutes);
 
 export default app;
