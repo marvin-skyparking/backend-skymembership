0a284ba55a5b872b5feafb51e1011d2b71885633
Marvin6262 <marvinaj12@gmail.com>
2025-01-14 09:55:19 +0700

    add membership

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -13,17 +13,16 @@ const EnvConfig: IEnvInterface = ENV;
 // }
 
 if (EnvConfig.NODE_ENV === 'development') {
-  EnvConfig.APP_URL = EnvConfig.APP_URL ?? 'http://localhost:9000';
-  EnvConfig.DB_DEV = EnvConfig.DB_DEV ?? 'db_skyccc';
-  EnvConfig.ENABLE_SWAGGER = EnvConfig.ENABLE_SWAGGER ?? true;
-  EnvConfig.PORT = EnvConfig.PORT ?? 9000;
-  EnvConfig.USERNAME_DB_DEV = EnvConfig.USERNAME_DB_DEV ?? 'root';
-  EnvConfig.PASSWORD_DB_DEV = EnvConfig.PASSWORD_DB_DEV ?? '50p4y5ky0v0!';
-  EnvConfig.SENTRY_URI = EnvConfig.SENTRY_URI ?? '';
-  EnvConfig.SENTRY_ENABLE = EnvConfig.SENTRY_ENABLE ?? false;
-  EnvConfig.JWT_SECRET = EnvConfig.JWT_SECRET ?? 'skyparking';
-  EnvConfig.BAYARIND_DEV_URL ?? 'https://snaptest.bayarind.id';
-  EnvConfig.PAYMENT_SERVICE_URL ??
-    'https://apipaymentservice.skyparking.online';
+  EnvConfig.APP_URL = EnvConfig.APP_URL;
+  EnvConfig.DB_DEV = EnvConfig.DB_DEV;
+  EnvConfig.ENABLE_SWAGGER = EnvConfig.ENABLE_SWAGGER;
+  EnvConfig.PORT = EnvConfig.PORT;
+  EnvConfig.USERNAME_DB_DEV = EnvConfig.USERNAME_DB_DEV;
+  EnvConfig.PASSWORD_DB_DEV = EnvConfig.PASSWORD_DB_DEV;
+  EnvConfig.SENTRY_URI = EnvConfig.SENTRY_URI;
+  EnvConfig.SENTRY_ENABLE = EnvConfig.SENTRY_ENABLE;
+  EnvConfig.JWT_SECRET = EnvConfig.JWT_SECRET;
+  EnvConfig.BAYARIND_DEV_URL = EnvConfig.JWT_SECRET;
+  EnvConfig.PAYMENT_SERVICE_URL = EnvConfig.PAYMENT_SERVICE_URL;
 }
 export default EnvConfig;
